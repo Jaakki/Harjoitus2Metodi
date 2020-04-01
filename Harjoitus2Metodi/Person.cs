f2fc1900e683ade14e3ae0322757e5ddf8d1b4b7
Jaakki <62286646+Jaakki@users.noreply.github.com>
2020-04-01 09:44:08 +0300

    Add project files.

---
+++ Harjoitus2Metodi/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Harjoitus2Metodi
+{
+    class Person
+    {
+        public string name;
+        public int age;
+        public List<string> Hobbies = new List<string>();
+        public Person friend;
+        bool Old = false;
+
+        public void Greet()
+        {
+            Console.WriteLine($"Terve! Miten menee?");
+        }
+
+        public void FormanGreet()
+        {
+            Console.WriteLine($"Terve, olen {name} ja olen {age} vuotta vanha.");
+        }
+
+        public void Ages()
+        {
+            {
+
+                if (age >= 18)
+                {
+                    Console.WriteLine($"Olen täysi-ikäinen.");
+                    Old = true;
+                }
+
+                else
+                {
+                    Console.WriteLine($"Olen alaikäinen.");
+                    Old = false;
+                }
+            }
+
+            {
+                if (Old == true)
+                {
+                    Console.WriteLine($"Olet vanha.");
+                }
+
+                else
+                {
+                    Console.WriteLine($"Olet nuori.");
+                }
+            }
+
+        }
+
+        public void GrowingOld(int years)
+        {
+            age += years;
+        }
+
+        public void Older()
+        {
+            if (friend == null)
+            {
+                Console.WriteLine($"{name} on yksinäinen.");
+            }
+
+            else if (age < friend.age)
+            {
+                Console.WriteLine($"{friend.name} on vanhempi kuin {name}.");
+            }
+
+            else
+            {
+                Console.WriteLine($"{name} on vanhempi kuin {friend.name}.");
+            }
+        }
+    }
+}
---
+++ Harjoitus2Metodi/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Harjoitus2Metodi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person per1 = new Person();
+            per1.name = "Jimbo";
+            per1.age = 27;
+            per1.Hobbies.Add("gambling");
+            per1.Hobbies.Add("drinking");
+            per1.Greet();
+            per1.FormanGreet();
+            per1.Ages();
+
+            Person per2 = new Person();
+            per2.name = "Bambo";
+            per2.age = 25;
+            per2.Hobbies.Add("gaming");
+            per2.Hobbies.Add("cycling");
+            per2.Ages();
+
+            Person per3 = new Person();
+            per3.name = "Ted";
+            per3.age = 13;
+            per3.Hobbies.Add("writing");
+            per3.Ages();
+
+            per1.friend = per2;                 //lisätään kaverit vasta täällä, koska ne pitää luoda ensin!
+            per2.friend = per1;
+
+            Console.WriteLine($"{per1.name} {per1.age} {per1.Hobbies[0]} {per1.Hobbies[1]}");
+            Console.WriteLine($"{per2.name} {per2.age} {per2.Hobbies[0]} {per2.Hobbies[1]}");
+            Console.WriteLine($"{per3.name} {per3.age} {per3.Hobbies[0]}");
+
+            per1.GrowingOld(5);
+            per2.GrowingOld(4);
+            per3.GrowingOld(3);
+
+            Console.WriteLine($"{per1.name} {per1.age} {per1.Hobbies[0]} {per1.Hobbies[1]}");
+            Console.WriteLine($"{per2.name} {per2.age} {per2.Hobbies[0]} {per2.Hobbies[1]}");
+            Console.WriteLine($"{per3.name} {per3.age} {per3.Hobbies[0]}");
+
+
+            per1.Older();
+            per2.Older();
+            per3.Older();
+
+            Console.ReadLine();
+        }
+    }
+}
